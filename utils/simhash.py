a00aa7d918360b9ebeffb55dae3383081eba2b73
SylvanasSun <sylvanassun_xtz@163.com>
2017-10-11 22:31:21 +0800

    Add module simhash.py

---
+++ utils/simhash.py
@@ -0,0 +1,9 @@
+# Created by SylvanasSun in 2017.10.11
+
+class Simhash(object):
+    """
+        Class Simhash implements simhash algorithms of the Google for filter duplicate content.
+        Simhash algorithms idea is will reduce the dimension of content and counts the
+        "Hamming Distance" the difference implements filter duplicate content.
+        About simhash algorithms the more introduction: https://en.wikipedia.org/wiki/SimHash
+    """
