8196cbf0b6fdb739b331045feb81a404aec9a587
SylvanasSun <sylvanassun_xtz@163.com>
2017-10-24 11:11:57 +0800

    Alter fish_crawler module: amend field of CommonItem in the items.py

--- fish_crawler/fish_crawler/items.py
+++ fish_crawler/fish_crawler/items.py
@@ -8,11 +8,13 @@
 import scrapy
 
 
-class FishCrawlerItem(scrapy.Item):
+class CommonItem(scrapy.Item):
     title = scrapy.Field()
     description = scrapy.Field()
     keywords = scrapy.Field()
+    p_texts = scrapy.Field()  # Text content in each tag <p>
     url = scrapy.Field()
+    date = scrapy.Field()  # Current page date,general get from Last-Modified in the response
     links = scrapy.Field()
     link_texts = scrapy.Field()  # Text content in each tag <a>
-    simhash = scrapy.Field() # Simhash code,depend title,description,keywords and link_texts
+    simhash = scrapy.Field()  # Simhash code,depend title,description,keywords,p_texts and link_texts
