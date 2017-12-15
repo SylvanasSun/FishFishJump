3be2aa9de1ec2838b697aac1689b7b031df22a58
SylvanasSun <sylvanassun_xtz@163.com>
2017-12-15 19:51:30 +0800

    Add function job_list() and update about cache function

--- fish_webapp/views/scrapyd.py
+++ fish_webapp/views/scrapyd.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, current_app
 from scrapyd.scrapyd_agent import ScrapydAgent
-from scrapyd.scrapyd_service import get_scrapyd_status
+from scrapyd.scrapyd_service import get_scrapyd_status, get_all_job_list
 
 scrapyd = Blueprint('scrapyd', __name__)
 
@@ -9,6 +9,7 @@ agent = None
 
 class CacheKeys():
     SCRAPYD_STATUS_KEY = 'scrapyd_status'
+    SCRAPYD_JOB_LIST = 'scrapyd_job_list'
 
 
 def fetch_scrapyd_agent(scrapyd_url):
@@ -18,13 +19,42 @@ def fetch_scrapyd_agent(scrapyd_url):
 
 @scrapyd.route('/status/chart', methods=['GET'])
 def scrapyd_status():
-    if current_app.config['ENABLE_CACHE']:
-        result = current_app.config['GLOBAL_CACHE'].get(CacheKeys.SCRAPYD_STATUS_KEY)
-        if result is None:
-            result = get_scrapyd_status(agent)
-            current_app.config['GLOBAL_CACHE'].set(CacheKeys.SCRAPYD_STATUS_KEY, result,
-                                                   timeout=current_app.config['CACHE_EXPIRE'])
-            return jsonify(result.__dict__)
+    if is_cacheable(current_app, CacheKeys.SCRAPYD_STATUS_KEY):
+        return jsonify(get_cached(current_app, CacheKeys.SCRAPYD_STATUS_KEY))
     else:
-        return jsonify(get_scrapyd_status(agent).__dict__)
+        result = get_scrapyd_status(agent).__dict__
+        set_cached(current_app, CacheKeys.SCRAPYD_STATUS_KEY, result)
+        return jsonify(result)
 
+
+@scrapyd.route('/job/list', methods=['GET'])
+def job_list():
+    if is_cacheable(current_app, CacheKeys.SCRAPYD_JOB_LIST):
+        return jsonify(get_cached(current_app, CacheKeys.SCRAPYD_JOB_LIST))
+    else:
+        result = generate_job_list_for_jsonify()
+        set_cached(current_app, CacheKeys.SCRAPYD_JOB_LIST, result)
+        return jsonify(result)
+
+
+def generate_job_list_for_jsonify():
+    pending, running, finished = get_all_job_list(agent)
+    result = {}
+    result['pending'] = [p.__dict__ for p in pending]
+    result['running'] = [r.__dict__ for r in running]
+    result['finished'] = [f.__dict__ for f in finished]
+    return result
+
+
+def is_cacheable(current_app, cache_key):
+    return current_app.config['ENABLE_CACHE'] and current_app.config['GLOBAL_CACHE'].get(cache_key) is not None
+
+
+def set_cached(current_app, cache_key, data):
+    if current_app.config['ENABLE_CACHE']:
+        current_app.config['GLOBAL_CACHE'].set(cache_key, data,
+                                               timeout=current_app.config['CACHE_EXPIRE'])
+
+
+def get_cached(current_app, cache_key):
+    return current_app.config['GLOBAL_CACHE'].get(cache_key)
