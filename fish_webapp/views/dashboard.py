21dd37fe8a4a52c5c43c64aa2a0d799dbc89036d
SylvanasSun <sylvanassun_xtz@163.com>
2017-12-18 17:15:34 +0800

    Add url mapping '/supervisor/scrapyd/job/list'

--- fish_webapp/views/dashboard.py
+++ fish_webapp/views/dashboard.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, url_for
+from utils.http_utils import request, RETURN_JSON
 
 dashboard = Blueprint('dashboard', __name__)
 
@@ -8,3 +9,8 @@ def home_page():
     if 'username' in session:
         return render_template('home.html', username=session['username'])
     return render_template('home.html')
+
+
+@dashboard.route('/job/list')
+def job_list_page():
+    return render_template('job_list.html')
