488de70eb71f3609194119ebe9b5cd9a9a2dfc61
SylvanasSun <sylvanassun_xtz@163.com>
2018-03-06 21:55:32 +0800

    [UPDATE] Modify name of the docker container and content of the setup.py

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ def read_requirements(filename):
             if not line.startswith('#')]
 
 
-packages = ['fish_core', 'fish_crawlers', 'fish_dashboard']
+packages = ['fish_core', 'fish_crawlers', 'fish_dashboard', 'fish_searcher']
 
 setup(
     name='FishFishJump',
     version=read_file('VERSION'),
-    description='Simple solution for search engines in the python',
-    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    description='FishFishJump is a solution that simply and basic for search engines and provide multiple demos that independent deployment by used Docker.',
+    long_description=read_file('README.rst'),
     author='SylvanasSun',
     author_email='sylvanas.sun@gmail.com',
     url='https://github.com/SylvanasSun/FishFishJump',
@@ -29,10 +29,11 @@ setup(
     install_requires=read_requirements('requirements.txt'),
     include_package_data=True,
     license='MIT',
-    keywords='FishFishJump python scrapy scrapy-redis',
+    keywords='FishFishJump python scrapy scrapy-redis search engines',
     entry_points={
         'console_scripts': [
-            'fish_dashboard=fish_dashboard.app:main'
+            'fish_dashboard=fish_dashboard.app:main',
+            'fish_searcher=fish_searcher.app:main'
         ]
     },
     classifiers=[
